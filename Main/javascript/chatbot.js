bea98c519073399cb05c4cd28283650248edca69
Luci <9022649@student.zadkine.nl>
2023-12-16 17:37:04 +0100

    Chatbot WIP 5
    Added favicon. to be added in main
    Changed styling to include favicon
    Continued writing chatbot responses

--- Main/javascript/chatbot.js
+++ Main/javascript/chatbot.js
@@ -1,106 +1,126 @@
 let userinput1 = document.getElementById('user-input-1')
-let userinput1text = userinput1.innerText;
 let userinput2 = document.getElementById('user-input-2');
-let userinput2classname = userinput2.className;
-let userinput2text = userinput2.innerText;
 let userinput3 = document.getElementById('user-input-3');
-let userinput3classname = userinput3.className;
-let userinput3text = userinput3.innerText;
 let userinput4 = document.getElementById('user-input-4');
-let userinput4classname = userinput4.className;
-let userinput4text = userinput4.innerText;
 let userinput5 = document.getElementById('user-input-5');
-let userinput5classname = userinput5.className;
-let userinput5text = userinput5.innerText;
 let tekstvraagbox = document.getElementById('tekst-vraag-box');
-let tekstvraagboxtext = tekstvraagbox.innerText;
 
-function reset(){
-    tekstvraagboxtext = "Waar gaat je vraag over?";
-    userinput1text = "Datum informatie";
+function Clearscreen(){
+    tekstvraagbox.innerText = "Waar gaat je vraag over?";
+    userinput1.innerText = "Datum informatie";
     userinput1.setAttribute("onclick", "datainformatie()");
-    userinput2text = "Informatie over software developer";
+    userinput2.innerText = "Informatie over software developer";
     userinput2.setAttribute("onclick", "softwaredev()");
-    userinput2classname = "tekst-vraag-userinput";
-    userinput3text = "";
+    userinput2.className = "tekst-vraag-userinput";
+    userinput3.innerText = "";
     userinput3.setAttribute("onclick", "");
-    userinput3classname = "tekst-vraag-userinput";
-    userinput4classname = "tekst-vraag-userinput";
-    userinput5classname = "tekst-vraag-userinput";
+    userinput3.className = "tekst-vraag-userinput";
+    userinput4.innerText = "";
+    userinput4.className = "tekst-vraag-userinput";
+    userinput5.innerText = "";
+    userinput5.className = "tekst-vraag-userinput";
 }
 
 function datainformatie(){
 // Change question in question-box and change answers for user-input
-    tekstvraagboxtext = "Welke data wil je weten";
-    userinput1text = "Wanneer begint het schooljaar";
+    tekstvraagbox.innerText = "Welke data wil je weten";
+    userinput1.innerText = "Wanneer begint het schooljaar";
     userinput1.setAttribute("onclick","schooljaar()");
-    userinput2text = "Wanneer is de opendag";
+    userinput2.innerText = "Wanneer is de opendag";
     userinput2.setAttribute("onclick", "opendag()");
-    userinput3text = "Will je terug naar het begin?";
-    userinput3.setAttribute("onclick", "reset()");
-    userinput4classname = "not-visible";
-    userinput5classname = "not-visible";
+    userinput3.innerText = "Will je terug naar het begin?";
+    userinput3.setAttribute("onclick", "Clearscreen()");
+    userinput4.className = "not-visible";
+    userinput5.className = "not-visible";
 }
 //
 function schooljaar() {
-    tekstvraagboxtext = "Het schooljaar begin Augustus 2024";
-    userinput1text = "Terug naar het begin";
-    userinput1.setAttribute("onclick", "reset()");
-    userinput2classname = "not-visible";
-    userinput3classname = "not-visible";
-    userinput4classname = "not-visible";
-    userinput5classname = "not-visible";
+    tekstvraagbox.innerText = "Het schooljaar begin Augustus 2024";
+    userinput1.innerText = "Terug naar het begin";
+    userinput1.setAttribute("onclick", "Clearscreen()");
+    userinput2.className = "not-visible";
+    userinput3.className = "not-visible";
+    userinput4.className = "not-visible";
+    userinput5.className = "not-visible";
 }
 function opendag(){
-    tekstvraagboxtext = "De opendag is op 26 en 27 januari 2024";
-    userinput1text = "Terug naar het begin";
-    userinput1.setAttribute("onclick", "reset()")
-    userinput2classname = "not-visible";
-    userinput3classname = "not-visible";
-    userinput4classname = "not-visible";
-    userinput5classname = "not-visible";
+    tekstvraagbox.innerText = "De opendag is op 26 en 27 januari 2024";
+    userinput1.innerText = "Terug naar het begin";
+    userinput1.setAttribute("onclick", "Clearscreen()")
+    userinput2.className = "not-visible";
+    userinput3.className = "not-visible";
+    userinput4.className = "not-visible";
+    userinput5.className = "not-visible";
 }
 function softwaredev() {
-    tekstvraagboxtext = "Wat wil je weten over de opleiding software-developer";
-    userinput1text = "Welke programmeer talen leren jullie?";
+    tekstvraagbox.innerText = "Wat wil je weten over de opleiding software-developer";
+    userinput1.innerText = "Welke programmeer talen leren jullie?";
     userinput1.setAttribute("onclick", "languages()");
-    userinput2text = "Hoe lang ben je bezig?";
+    userinput2.innerText = "Hoe lang ben je bezig?";
     userinput2.setAttribute("onclick", "tijdopleiding()");
-    userinput3text = "Wat ga je maken als softwaredeveloper";
+    userinput3.innerText = "Wat ga je maken als softwaredeveloper";
     userinput3.setAttribute("onclick", "infodev()");
-    userinput4text = "Heb je al besloten om je in te schrijven voor de opleiding?"
+    userinput4.innerText = "Heb je al besloten om je in te schrijven voor de opleiding?"
     userinput4.setAttribute("onclick", "inschrijven()");
-    userinput5text = "Wil je terug naar het begin?";
-    userinput5.setAttribute("onclick", "reset()");
+    userinput5.innerText = "Wil je terug naar het begin?";
+    userinput5.setAttribute("onclick", "Clearscreen()");
 
 }
 function languages(){
-    tekstvraagboxtext = "Bij deze opleiding zul je leren over de volgende talen";
-    userinput1text = "Html/Css";
+    tekstvraagbox.innerText = "Bij deze opleiding zul je leren over de volgende talen";
+    userinput1.innerText = "Html/Css";
     userinput1.setAttribute("onclick", "HtmlCss()");
-    userinput2text = "Javascript";
+    userinput2.innerText = "Javascript";
     userinput2.setAttribute("onclick", "Javascript()");
-    userinput3text = "PhP";
+    userinput3.innerText = "PhP";
     userinput3.setAttribute("onclick", "PhP()");
-    userinput4classname = "not-visible";
-    userinput5classname = "not-visible";
+    userinput4.className = "not-visible";
+    userinput5.className = "not-visible";
 }
 function HtmlCss(){
-    tekstvraagboxtext = "Dit zijn de eerste talen die je gaat leren. Html en CSS vormen de basis van het developen van websites. Als je meer wilt weten klik dan op een van de knoppen";
-    userinput1text = "Html";
+    tekstvraagbox.innerText = "Dit zijn de eerste talen die je gaat leren. Html en CSS vormen de basis van het developen van websites. Als je meer wilt weten klik dan op een van de knoppen voor Html of Css.";
+    userinput1.innerText = "Html";
     userinput1.setAttribute("onclick", "location.href='https://www.w3schools.com/html/'");
-    userinput2text = "Css";
+    userinput2.innerText = "Css";
     userinput2.setAttribute("onclick", "location.href='https://www.w3schools.com/css/default.asp'");
-    userinput3.setAttribute("onclick", "reset()");
-    userinput4classname = "not-visible";
-    userinput5classname = "not-visible";
+    userinput3.innerText = "Terug naar het begin";
+    userinput3.setAttribute("onclick", "Clearscreen()");
+    userinput4.className = "not-visible";
+    userinput5.className = "not-visible";
+}
+function Javascript() {
+    tekstvraagbox.innerText = "Dit zul je in je 2de periode leren. Javascript zorgt ervoor dat een website interactief is. Deze chatbot is volledig geschreven met javascript, html en css. Als je meer wilt weten klik dan op de knop Javascript.";
+    userinput1.innerText = "Javascript";
+    userinput1.setAttribute("onclick", "location.href='https://www.w3schools.com/js/default.asp'");
+    userinput2.innerText = "Terug naar het begin";
+    userinput2.setAttribute("onclick", "Clearscreen()")
+    userinput3.className = "not-visible";
+    userinput4.className = "not-visible";
+    userinput5.className = "not-visible";
+}
+function PhP(){
+    tekstvraagbox.innerText = "Dit zul je in de 3de of 4de periode leren. PhP zorgt ervoor dat websites zoals forums en webshops gebruik kunnen maken van een database voor allerlei dingen zoals het inloggen, het bijhouden van de vooraad en de prijs. Als je meer wilt weten klik dan op een van de knoppen voor PhP of Databases";
+    userinput1.innerText = "PhP";
+    userinput1.setAttribute("onclick", "location.href='https://www.w3schools.com/php/default.asp'")
+    userinput2.innerText = "Databases";
+    userinput2.setAttribute("onclick", "location.href='https://nl.wikipedia.org/wiki/Database'");
+    userinput3.innerText = "terug naar het begin";
+    userinput3.setAttribute("onclick", "Clearscreen()")
+    userinput4.className = "not-visible";
+    userinput5.className = "not-visible";
 }
 function tijdopleiding(){
-    tekstvraagboxtext = "Deze opleiding duurt 4 jaar met een mogelijkheid om het in 3 jaar te doen";
-    userinput1text = "Terug naar het begin";
-    userinput1.setAttribute("onclick", "reset()");
-    userinput2classname = "not-visible";
-    userinput3classname = "not-visible";
-    userinput4classname = "not-visible";
-    userinput5classname = "not-visible";
+    tekstvraagbox.innerText = "Deze opleiding duurt 4 jaar met een mogelijkheid om het in 3 jaar te doen";
+    userinput1.innerText = "Terug naar het begin";
+    userinput1.setAttribute("onclick", "Clearscreen()");
+    userinput2.className = "not-visible";
+    userinput3.className = "not-visible";
+    userinput4.className = "not-visible";
+    userinput5.className = "not-visible";
+}
+function infodev(){
+    tekstvraagbox.innerText = "";
+}
+function inschrijven() {
+    tekstvraagbox.innerText = "";
 }
