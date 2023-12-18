4d84f871350e555348340493cdb52e430e1b33b9
Luci <9022649@student.zadkine.nl>
2023-12-18 16:41:54 +0100

    Chatbot WIP 8
    Fixed issue that made the go back function not work

--- Main/javascript/chatbot.js
+++ Main/javascript/chatbot.js
@@ -1,8 +1,9 @@
 let userinput1 = document.getElementById('user-input-1'); // Gets user input 1 button element
-let userinput2 = document.getElementById('user-input-2'); // Gets user input 2 button element
-let userinput3 = document.getElementById('user-input-3'); // Gets user input 3 button element
-let userinput4 = document.getElementById('user-input-4'); // Gets user input 4 button element
-let userinput5 = document.getElementById('user-input-5'); // Gets user input 5 button element
+let userinput2 = document.getElementById('user-input-2');
+let userinput3 = document.getElementById('user-input-3');
+let userinput4 = document.getElementById('user-input-4');
+let userinput5 = document.getElementById('user-input-5');
+let userinput6 = document.getElementById('user-input-6');
 let tekstvraagbox = document.getElementById('tekst-vraag-box'); // Gets the question-box element
 let gobackint = 0; // variable to check where user is for goback function to work
 
@@ -27,6 +28,9 @@ function goback(userinputname1,userinputname2){
     if (gobackint === 4){
         userinputname1.setAttribute("onclick", "languages()")
     }
+    if (gobackint === 5){
+        userinputname1.setAttribute("onclick", "infodev()");
+    }
     userinputname2.innerText = "Terug naar het begin";
     userinputname2.className = "tekst-vraag-userinput";
     userinputname2.setAttribute("onclick", "clearscreen()");
@@ -43,10 +47,9 @@ function clearscreen(){
     userinput3.innerText = "Heb je al besloten om je in te schrijven voor de opleiding?";
     userinput3.className = "tekst-vraag-userinput";
     userinput3.setAttribute("onclick", "inschrijvenvraagbox()");
-    userinput4.innerText = "";
-    userinput4.className = "tekst-vraag-userinput";
-    userinput5.innerText = "";
-    userinput5.className = "tekst-vraag-userinput";
+    userinput4.className = "not-visible";
+    userinput5.className = "not-visible";
+    userinput6.className = "not-visible";
 }
 
 // Function to display information about dates
@@ -60,12 +63,13 @@ function dateinfo(){
     userinput2.innerText = "Wanneer is de opendag";
     userinput2.setAttribute("onclick", "datesinfo(2)");
     userinput5.className = "not-visible";
+    userinput6.className = "not-visible";
 }
 
 // Function to display information about start school year and opendag
 function datesinfo(userinputintfordatum) {
-    goback(userinput1,userinput2)
     gobackint = 2;
+    goback(userinput1,userinput2)
     if(userinputintfordatum === 1) {
         tekstvraagbox.innerText = "Het schooljaar begin Augustus 2024";
     }
@@ -74,21 +78,25 @@ function datesinfo(userinputintfordatum) {
     userinput3.className = "not-visible";
     userinput4.className = "not-visible";
     userinput5.className = "not-visible";
+    userinput6.className = "not-visible";
 }
 
 // Function to display information about the software developer study
 function softwaredev() {
     gobackint = 1;
-    goback(userinput4,userinput5)
+    goback(userinput5,userinput6)
     tekstvraagbox.innerText = "Wat wil je weten over de opleiding software-developer";
     userinput1.innerText = "Welke programmeer talen leren jullie?";
     userinput1.setAttribute("onclick", "languages()");
     userinput2.innerText = "Hoe lang ben je bezig?";
-    userinput2.className = "tekst-vraag-userinput"
+    userinput2.className = "tekst-vraag-userinput";
     userinput2.setAttribute("onclick", "tijdopleiding()");
     userinput3.innerText = "Wat ga je maken als softwaredeveloper";
-    userinput3.className = "tekst-vraag-userinput"
+    userinput3.className = "tekst-vraag-userinput";
     userinput3.setAttribute("onclick", "infodev()");
+    userinput4.innerText = "Hoe zit het met stages?";
+    userinput4.className = "tekst-vraag-userinput";
+    userinput4.setAttribute("onclick", "stage()");
 }
 
 // Function that displays information about the languages learned during the study
@@ -102,6 +110,7 @@ function languages(){
     userinput2.setAttribute("onclick", "Javascript()");
     userinput3.innerText = "PhP";
     userinput3.setAttribute("onclick", "PhP()");
+    userinput6.className = "not-visible"
 }
 
 // Function that displays information about Html and Css
@@ -110,10 +119,11 @@ function HtmlCss(){
     goback(userinput3, userinput4);
     tekstvraagbox.innerText = "Dit zijn de eerste talen die je gaat leren. Html en CSS vormen de basis van het developen van websites. Als je meer wilt weten klik dan op een van de knoppen voor Html of Css.";
     userinput1.innerText = "Html";
-    userinput1.setAttribute("onclick", "location.href='https://www.w3schools.com/html/'");
+    userinput1.setAttribute("onclick", "window.open('https://www.w3schools.com/html/')");
     userinput2.innerText = "Css";
-    userinput2.setAttribute("onclick", "location.href='https://www.w3schools.com/css/default.asp'");
+    userinput2.setAttribute("onclick", "window.open('https://www.w3schools.com/css/default.asp')");
     userinput5.className = "not-visible";
+    userinput6.className = "not-visible";
 }
 
 // Function that displays information about Javascript
@@ -122,9 +132,10 @@ function Javascript() {
     goback(userinput2,userinput3)
     tekstvraagbox.innerText = "Dit zul je in je 2de periode leren. Javascript zorgt ervoor dat een website interactief is. Deze interactie is volledig geschreven met javascript, html en css. Als je meer wilt weten klik dan op de knop Javascript.";
     userinput1.innerText = "Javascript";
-    userinput1.setAttribute("onclick", "location.href='https://www.w3schools.com/js/default.asp'");
+    userinput1.setAttribute("onclick", "window.open('https://www.w3schools.com/js/default.asp')");
     userinput4.className = "not-visible";
     userinput5.className = "not-visible";
+    userinput6.className = "not-visible";
 }
 
 // Function that displays information about PhP
@@ -133,10 +144,11 @@ function PhP(){
     goback(userinput3,userinput4);
     tekstvraagbox.innerText = "Dit zul je in de 3de of 4de periode leren. PhP zorgt ervoor dat websites zoals forums en webshops gebruik kunnen maken van een database voor allerlei dingen zoals het inloggen, het bijhouden van de vooraad en de prijs. Als je meer wilt weten klik dan op een van de knoppen voor PhP of Databases";
     userinput1.innerText = "PhP";
-    userinput1.setAttribute("onclick", "location.href='https://www.w3schools.com/php/default.asp'");
+    userinput1.setAttribute("onclick", "window.open('https://www.w3schools.com/php/default.asp')");
     userinput2.innerText = "Databases";
-    userinput2.setAttribute("onclick", "location.href='https://nl.wikipedia.org/wiki/Database'");
+    userinput2.setAttribute("onclick", "window.open('https://nl.wikipedia.org/wiki/Database')");
     userinput5.className = "not-visible";
+    userinput6.className = "not-visible";
 }
 
 // Function that displays information about how long this study takes
@@ -147,19 +159,67 @@ function tijdopleiding(){
     userinput3.className = "not-visible";
     userinput4.className = "not-visible";
     userinput5.className = "not-visible";
+    userinput6.className = "not-visible"
 }
 
 // Function that displays information about what you learn to make during the study
 function infodev(){
     gobackint = 3;
     goback(userinput4,userinput5);
-    tekstvraagbox.innerText = "Deze dingen ga je leren als software-developer";
+    tekstvraagbox.innerText = "Deze dingen ga je leren maken als software-developer";
     userinput1.innerText = "Websites";
     userinput1.setAttribute("onclick", "websites()");
     userinput2.innerText = "Databases";
     userinput2.setAttribute("onclick", "databases()");
     userinput3.innerText = "Games";
+    userinput3.className = "tekst-vraag-userinput";
     userinput3.setAttribute("onclick", "games()");
+    userinput6.className = "not-visible";
+}
+
+// Function that displays information about website development
+function websites(){
+    gobackint = 5;
+    goback(userinput1,userinput2);
+    tekstvraagbox.innerText = "Websites, je ziet ze overal. Als software developer leer jij hoe je de beste websites kunt maken. Van Html tot PhP. jij leert het allemaal.";
+    userinput3.className = "not-visible";
+    userinput4.className = "not-visible";
+    userinput5.className = "not-visible";
+    userinput6.className = "not-visible";
+}
+
+// Function that displays information about databases
+function databases(){
+    gobackint = 5;
+    goback(userinput1,userinput2);
+    tekstvraagbox.innerText = "Databases zijn een belangrijk onderdeel voor websites. Ze houden bij wat de prijs van een artikel is. Hoeveel er nog op vooraad is. Gebruikersgegevens. Al deze gegevens worden bijgehouden en opgeslagen in de databases van de websites.";
+    userinput3.className = "not-visible";
+    userinput4.className = "not-visible";
+    userinput5.className = "not-visible";
+    userinput6.className = "not-visible";
+}
+
+// Function that displays information about game development
+function games(){
+    gobackint = 5;
+    goback();
+    tekstvraagbox.innerText = "Je hebt tijdens de opleiding de mogelijkheid om game development als keuzevak te doen. Je zal dan een basis leren in game development";
+    userinput3.className = "not-visible";
+    userinput4.className = "not-visible";
+    userinput5.className = "not-visible";
+    userinput6.className = "not-visible";
+
+}
+
+// Function that displays information about internship
+function stage(){
+    gobackint = 3;
+    goback(userinput1,userinput2);
+    tekstvraagbox.innerText = "Je vraagt je dus af hoe het zit met stages. Je zal in je 2de jaar een half jaar stage moeten gaan lopen. Ook in je 3de leerjaar loop je een half jaar stage. Je zal hier aan het begin van je schooljaar wat meer over horen.";
+    userinput3.className = "not-visible";
+    userinput4.className = "not-visible";
+    userinput5.className = "not-visible";
+    userinput6.className = "not-visible";
 }
 
 // Function that displays information about registering to study
@@ -171,4 +231,5 @@ function inschrijvenvraagbox() {
     userinput1.setAttribute("onclick", "window.open('https://www.techniekcollegerotterdam.nl/aanmelden');");
     userinput4.className = "not-visible";
     userinput5.className = "not-visible";
+    userinput6.className = "not-visible";
 }
