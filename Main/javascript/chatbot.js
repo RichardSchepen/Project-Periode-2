a7327b5ae63beb019804d534f7c777e9530d9e24
Luci <9022649@student.zadkine.nl>
2023-12-18 08:14:10 +0100

    Chatbot WIP 7
    Reworked function to go back a question.
    Changed location of register question.

--- Main/javascript/chatbot.js
+++ Main/javascript/chatbot.js
@@ -1,72 +1,100 @@
-let userinput1 = document.getElementById('user-input-1')
-let userinput2 = document.getElementById('user-input-2');
-let userinput3 = document.getElementById('user-input-3');
-let userinput4 = document.getElementById('user-input-4');
-let userinput5 = document.getElementById('user-input-5');
-let tekstvraagbox = document.getElementById('tekst-vraag-box');
+let userinput1 = document.getElementById('user-input-1'); // Gets user input 1 button element
+let userinput2 = document.getElementById('user-input-2'); // Gets user input 2 button element
+let userinput3 = document.getElementById('user-input-3'); // Gets user input 3 button element
+let userinput4 = document.getElementById('user-input-4'); // Gets user input 4 button element
+let userinput5 = document.getElementById('user-input-5'); // Gets user input 5 button element
+let tekstvraagbox = document.getElementById('tekst-vraag-box'); // Gets the question-box element
+let gobackint = 0; // variable to check where user is for goback function to work
 
+/* Goback is used for when a user wants to go back a question or when a user wants to go back to the beginning.
+   The int is used to keep track of where the user is so the user gets sent to the right questions*/
+function goback(userinputname1,userinputname2){
+    userinputname1.innerText = "Terug naar de vorige vraag";
+    userinputname1.className = "tekst-vraag-userinput";
+    // Makes the user go back to the first question
+    if (gobackint === 1){
+        userinputname1.setAttribute("onclick", "clearscreen()")
+    }
+    // Makes the user go back to the date questions
+    if (gobackint === 2){
+        userinputname1.setAttribute("onclick" , "dateinfo()");
+    }
+    // Makes the user go back to the software development questions
+    if (gobackint === 3){
+        userinputname1.setAttribute("onclick", "softwaredev()");
+    }
+    // Makes the user go back to the languages questions
+    if (gobackint === 4){
+        userinputname1.setAttribute("onclick", "languages()")
+    }
+    userinputname2.innerText = "Terug naar het begin";
+    userinputname2.className = "tekst-vraag-userinput";
+    userinputname2.setAttribute("onclick", "clearscreen()");
+}
+
+// Clears the screen for new interaction
 function clearscreen(){
     tekstvraagbox.innerText = "Waar gaat je vraag over?";
     userinput1.innerText = "Datum informatie";
-    userinput1.setAttribute("onclick", "datainformatie()");
+    userinput1.setAttribute("onclick", "dateinfo()");
     userinput2.innerText = "Informatie over software developer";
     userinput2.setAttribute("onclick", "softwaredev()");
     userinput2.className = "tekst-vraag-userinput";
-    userinput3.innerText = "";
-    userinput3.setAttribute("onclick", "");
+    userinput3.innerText = "Heb je al besloten om je in te schrijven voor de opleiding?";
     userinput3.className = "tekst-vraag-userinput";
+    userinput3.setAttribute("onclick", "inschrijvenvraagbox()");
     userinput4.innerText = "";
     userinput4.className = "tekst-vraag-userinput";
     userinput5.innerText = "";
     userinput5.className = "tekst-vraag-userinput";
 }
 
-function datainformatie(){
-// Change question in question-box and change answers for user-input
-    tekstvraagbox.innerText = "Welke data wil je weten";
+// Function to display information about dates
+function dateinfo(){
+// Changes question in question-box and change answers for user-input
+    gobackint = 1; // Assign before goback or it won't work
+    goback(userinput3,userinput4); // Returns user to previous question
+    tekstvraagbox.innerText = "Welke datum wil je weten";
     userinput1.innerText = "Wanneer begint het schooljaar";
-    userinput1.setAttribute("onclick","schooljaar()");
+    userinput1.setAttribute("onclick","datesinfo(1)");
     userinput2.innerText = "Wanneer is de opendag";
-    userinput2.setAttribute("onclick", "opendag()");
-    userinput3.innerText = "Will je terug naar het begin?";
-    userinput3.setAttribute("onclick", "clearscreen()");
-    userinput4.className = "not-visible";
+    userinput2.setAttribute("onclick", "datesinfo(2)");
     userinput5.className = "not-visible";
 }
-//
-function schooljaar() {
-    tekstvraagbox.innerText = "Het schooljaar begin Augustus 2024";
-    userinput1.innerText = "Terug naar het begin";
-    userinput1.setAttribute("onclick", "clearscreen()");
-    userinput2.className = "not-visible";
+
+// Function to display information about start school year and opendag
+function datesinfo(userinputintfordatum) {
+    goback(userinput1,userinput2)
+    gobackint = 2;
+    if(userinputintfordatum === 1) {
+        tekstvraagbox.innerText = "Het schooljaar begin Augustus 2024";
+    }
+    else if(userinputintfordatum === 2) {
+        tekstvraagbox.innerText = "De opendag is op 26 en 27 januari 2024";}
     userinput3.className = "not-visible";
     userinput4.className = "not-visible";
     userinput5.className = "not-visible";
 }
-function opendag(){
-    tekstvraagbox.innerText = "De opendag is op 26 en 27 januari 2024";
-    userinput1.innerText = "Terug naar het begin";
-    userinput1.setAttribute("onclick", "clearscreen()")
-    userinput2.className = "not-visible";
-    userinput3.className = "not-visible";
-    userinput4.className = "not-visible";
-    userinput5.className = "not-visible";
-}
+
+// Function to display information about the software developer study
 function softwaredev() {
+    gobackint = 1;
+    goback(userinput4,userinput5)
     tekstvraagbox.innerText = "Wat wil je weten over de opleiding software-developer";
     userinput1.innerText = "Welke programmeer talen leren jullie?";
     userinput1.setAttribute("onclick", "languages()");
     userinput2.innerText = "Hoe lang ben je bezig?";
+    userinput2.className = "tekst-vraag-userinput"
     userinput2.setAttribute("onclick", "tijdopleiding()");
     userinput3.innerText = "Wat ga je maken als softwaredeveloper";
+    userinput3.className = "tekst-vraag-userinput"
     userinput3.setAttribute("onclick", "infodev()");
-    userinput4.innerText = "Heb je al besloten om je in te schrijven voor de opleiding?"
-    userinput4.setAttribute("onclick", "inschrijvenvraagbox()");
-    userinput5.innerText = "Wil je terug naar het begin?";
-    userinput5.setAttribute("onclick", "clearscreen()");
-
 }
+
+// Function that displays information about the languages learned during the study
 function languages(){
+    gobackint = 3;
+    goback(userinput4, userinput5);
     tekstvraagbox.innerText = "Bij deze opleiding zul je leren over de volgende talen";
     userinput1.innerText = "Html/Css";
     userinput1.setAttribute("onclick", "HtmlCss()");
@@ -74,53 +102,57 @@ function languages(){
     userinput2.setAttribute("onclick", "Javascript()");
     userinput3.innerText = "PhP";
     userinput3.setAttribute("onclick", "PhP()");
-    userinput4.innerText = "Terug naar vorige vraag";
-    userinput4.setAttribute("onclick", "softwaredev()");
-    userinput5.innerText = "Terug naar het begin";
-    userinput5.setAttribute("onclick", "clearscreen()");
 }
+
+// Function that displays information about Html and Css
 function HtmlCss(){
+    gobackint = 4;
+    goback(userinput3, userinput4);
     tekstvraagbox.innerText = "Dit zijn de eerste talen die je gaat leren. Html en CSS vormen de basis van het developen van websites. Als je meer wilt weten klik dan op een van de knoppen voor Html of Css.";
     userinput1.innerText = "Html";
     userinput1.setAttribute("onclick", "location.href='https://www.w3schools.com/html/'");
     userinput2.innerText = "Css";
     userinput2.setAttribute("onclick", "location.href='https://www.w3schools.com/css/default.asp'");
-    userinput3.innerText = "Terug naar het begin";
-    userinput3.setAttribute("onclick", "clearscreen()");
-    userinput4.className = "not-visible";
     userinput5.className = "not-visible";
 }
+
+// Function that displays information about Javascript
 function Javascript() {
-    tekstvraagbox.innerText = "Dit zul je in je 2de periode leren. Javascript zorgt ervoor dat een website interactief is. Deze chatbot is volledig geschreven met javascript, html en css. Als je meer wilt weten klik dan op de knop Javascript.";
+    gobackint = 4;
+    goback(userinput2,userinput3)
+    tekstvraagbox.innerText = "Dit zul je in je 2de periode leren. Javascript zorgt ervoor dat een website interactief is. Deze interactie is volledig geschreven met javascript, html en css. Als je meer wilt weten klik dan op de knop Javascript.";
     userinput1.innerText = "Javascript";
     userinput1.setAttribute("onclick", "location.href='https://www.w3schools.com/js/default.asp'");
-    userinput2.innerText = "Terug naar het begin";
-    userinput2.setAttribute("onclick", "clearscreen()")
-    userinput3.className = "not-visible";
     userinput4.className = "not-visible";
     userinput5.className = "not-visible";
 }
+
+// Function that displays information about PhP
 function PhP(){
+    gobackint = 4;
+    goback(userinput3,userinput4);
     tekstvraagbox.innerText = "Dit zul je in de 3de of 4de periode leren. PhP zorgt ervoor dat websites zoals forums en webshops gebruik kunnen maken van een database voor allerlei dingen zoals het inloggen, het bijhouden van de vooraad en de prijs. Als je meer wilt weten klik dan op een van de knoppen voor PhP of Databases";
     userinput1.innerText = "PhP";
-    userinput1.setAttribute("onclick", "location.href='https://www.w3schools.com/php/default.asp'")
+    userinput1.setAttribute("onclick", "location.href='https://www.w3schools.com/php/default.asp'");
     userinput2.innerText = "Databases";
     userinput2.setAttribute("onclick", "location.href='https://nl.wikipedia.org/wiki/Database'");
-    userinput3.innerText = "Terug naar het begin";
-    userinput3.setAttribute("onclick", "clearscreen()")
-    userinput4.className = "not-visible";
     userinput5.className = "not-visible";
 }
+
+// Function that displays information about how long this study takes
 function tijdopleiding(){
+    gobackint = 3;
+    goback(userinput1,userinput2);
     tekstvraagbox.innerText = "Deze opleiding duurt 4 jaar met een mogelijkheid om het in 3 jaar te doen";
-    userinput1.innerText = "Terug naar het begin";
-    userinput1.setAttribute("onclick", "clearscreen()");
-    userinput2.className = "not-visible";
     userinput3.className = "not-visible";
     userinput4.className = "not-visible";
     userinput5.className = "not-visible";
 }
+
+// Function that displays information about what you learn to make during the study
 function infodev(){
+    gobackint = 3;
+    goback(userinput4,userinput5);
     tekstvraagbox.innerText = "Deze dingen ga je leren als software-developer";
     userinput1.innerText = "Websites";
     userinput1.setAttribute("onclick", "websites()");
@@ -128,22 +160,15 @@ function infodev(){
     userinput2.setAttribute("onclick", "databases()");
     userinput3.innerText = "Games";
     userinput3.setAttribute("onclick", "games()");
-    userinput4.innerText = "Terug naar het begin";
-    userinput4.setAttribute("onclick", "clearscreen()");
-    userinput5.className = "not-visible";
-    
 }
+
+// Function that displays information about registering to study
 function inschrijvenvraagbox() {
+    gobackint = 1;
+    goback(userinput2,userinput3);
     tekstvraagbox.innerText = "Tof dat je je wilt inschrijven. En maak je geen zorgen je kan je voor elke opleiding inschrijven. Mocht je dus eigenlijk al een andere opleiding hebben gekozen dan kan je je alsnog daarvoor inschrijven.";
-    userinput1.innerText = "Inschrijven"
-    userinput1.setAttribute("onclick", "inschrijvenweb()");
-    userinput2.innerText = "Terug naar de vorige vraag";
-    userinput2.setAttribute("onclick", "softwaredev()");
-    userinput3.innerText = "Terug naar het begin";
-    userinput3.setAttribute("onclick", "clearscreen()")
+    userinput1.innerText = "Inschrijven";
+    userinput1.setAttribute("onclick", "window.open('https://www.techniekcollegerotterdam.nl/aanmelden');");
     userinput4.className = "not-visible";
     userinput5.className = "not-visible";
-}
-function inschrijvenweb(){
-    window.open("https://www.techniekcollegerotterdam.nl/aanmelden");
 }
